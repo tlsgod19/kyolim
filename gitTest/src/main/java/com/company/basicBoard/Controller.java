01faccb63772ed1a2d498057c8e2e425c34480fc
alwaysconfidentz <tlswlsdn789@naver.com>
2024-03-07 16:33:58 +0900

    뀨

--- gitTest/src/main/java/com/company/basicBoard/Controller.java
+++ gitTest/src/main/java/com/company/basicBoard/Controller.java
@@ -2,12 +2,22 @@ package com.company.basicBoard;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @org.springframework.stereotype.Controller
 public class Controller {
 	@GetMapping(value ="/test")
-	public String home() {
+	public String home() {		
 		return "Test";
 	}
+
+	@GetMapping(value ="/test2")
+	public String test2() {
+		return "";
+	}
+	
 }
